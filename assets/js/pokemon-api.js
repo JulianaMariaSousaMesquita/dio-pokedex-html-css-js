818ad097638f1f5c972b927aafaa78ce48afc32d
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-11-12 22:38:52 -0300

    :sparkles: feat: Converting API model to own model

--- assets/js/pokemon-api.js
+++ assets/js/pokemon-api.js
@@ -1,12 +1,30 @@
 const pokemonApi = {}
 
+function convertPokeApiDetailToPokemon(pokemonDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokemonDetail.order
+    pokemon.name = pokemonDetail.name
+    pokemon.types = pokemonDetail.types.map((typeSlot) => {
+        typeSlot.type.name
+    })
+    pokemon.type = pokemonDetail.types.get(0)
+    pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default
+    return pokemon
+}
+
+pokemonApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then((pokemon) => convertPokeApiDetailToPokemon(pokemon))
+}
+
 pokemonApi.getPokemons = ( offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => console.log(jsonBody.results))
-    .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url).then((response) => response.json())))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails)
 }
