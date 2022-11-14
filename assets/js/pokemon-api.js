5a9cc5ce9dd2dc3d306210fc3893d086aab1e656
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-11-14 13:29:42 -0300

    :bug: fix: Changing order number by id

--- assets/js/pokemon-api.js
+++ assets/js/pokemon-api.js
@@ -2,7 +2,7 @@ const pokemonApi = {}
 
 function convertPokeApiDetailToPokemon(pokemonDetail){
     const pokemon = new Pokemon()
-    pokemon.number = pokemonDetail.order
+    pokemon.number = pokemonDetail.id
     pokemon.name = pokemonDetail.name
     pokemon.types = pokemonDetail.types.map((typeSlot) => {
         typeSlot.type.name
